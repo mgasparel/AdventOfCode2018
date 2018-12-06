a26282fc76843cfdff9b711bae1789c8d9975d4f
Mike Gasparelli <magasparel@gmail.com>
2018-12-05 21:56:03 -0500

    Day 1 Part 1

---
+++ day_01/day-01-p1/Day_01_p1.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    let path = "Day-01-p1-input.txt"
+
+    let lines = System.IO.File.ReadLines(path)
+
+    let s = Seq.sumBy int lines
+
+    printf "%i" s
+
+    0
